33555a22b2acacfec85f5bd9226743881d0f319e
Anur4ik <your_email@whatever.com>
2025-09-21 10:27:15 +0300

    created function list of worker

---
+++ Independent task №2/data_input.py
@@ -0,0 +1,11 @@
+def input_worker():
+    worker = {}
+    while True:
+        name = input("Введіть ім'я співробітника кількість відпрацьованих днів або 'stop' щоб закінчити: ")
+        if name.lower() == 'stop':
+            break
+        salary =float (input("Введіть заробітню плату цього співробітника : "))
+        days_worked =  int(input("Введіть кількість відпрацьованих днів : "))
+        worker[name] = {'Зарплата': salary, 'Дні': days_worked}
+
+    return worker
