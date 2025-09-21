b5c0f5ce465ea83ff0964911b882baa9681e44a0
Anur4ik <your_email@whatever.com>
2025-09-21 10:41:58 +0300

    updated function initialize worker

--- Independent task №2/data_input.py
+++ Independent task №2/data_input.py
@@ -4,9 +4,8 @@ def input_worker():
         name = input("Введіть ім'я співробітника або 'stop' щоб закінчити: ")
         if name.lower() == 'stop':
             break
-        salary =float (input("Введіть заробітню плату цього співробітника : "))
-        days_worked =  int(input("Введіть кількість відпрацьованих днів : "))
+        salary = float(input("Введіть заробітню плату цього співробітника : "))
+        days_worked =int(input("Введіть кількість відпрацьованих днів : "))
         worker[name] = {'Зарплата': salary, 'Дні': days_worked}
-
 
     return worker
