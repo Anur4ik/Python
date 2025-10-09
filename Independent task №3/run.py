6ebc197ebeebf6e3f625f97677bf51f65d75b7e6
nasty <anonim@gmail.com>
2025-10-09 08:27:27 +0300

    updated run.py independent task 3

--- Independent task №3/run.py
+++ Independent task №3/run.py
@@ -1,15 +1,20 @@
 from employee import Employee
 from manager import Manager
 
-employee1 = Employee("Іван", 15000, 20, 10)
-employee2 = Employee("Марія", 18000, 25, 5)
-
-manager1 = Manager("Олена", 25000, 26, 15, 5)
-manager2 = Manager("Андрій", 30000, 28, 20, 8)
+employee1 = Employee("Олексій", 15000, 20, 10)
+employee2 = Employee("Денис", 15000, 25, 5)
+employee3 = Employee("Владислав", 15300, 25, 5)
+manager1 = Manager("Олександр", 40000, 15, 15, 10)
+manager2 = Manager("Андрій", 25000, 20, 20, 8)
 
 team = [employee1, employee2, manager1, manager2]
 
 for i in team:
-    print(f"Працівник: {i.get_name()}")
+    if isinstance(i, Manager):
+        print(f"Менджер : {i.get_name()}")
+        print(i.zvit())
+    else:
+        print(f"Працівник: {i.get_name()}")
     print(f"Зарплата: {i.calculate_monthly_salary():.2f} грн")
     print(f"Бонус: {i.bonus():.2f} грн")
+    print("-------------------------------")
