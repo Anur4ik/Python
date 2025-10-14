733daf17bf6d50aeca291736e6d81ea059a93e57
nasty <anonim@gmail.com>
2025-10-14 09:00:57 +0300

    added all methods to main file

--- pr4/main.py
+++ pr4/main.py
@@ -16,8 +16,6 @@ canvas.pack()
 
 score = Score(canvas)
 catcher = Catcher(canvas, 'blue', score)
-tk.update()
-time.sleep(1)
 eggs = []
 while 1:
     if random.randint(1, 100) == 1:
@@ -25,14 +23,15 @@ while 1:
     for egg in list(eggs):
         if egg.draw() == 'hit bottom':
             eggs.remove(egg)
+    catcher.catch(eggs)
+    catcher.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+    if score.lost >= 5:
+        break
 
+canvas.create_text(250, 200, text="Гра завершена!", font=('Helvetica', 30), fill='red')
+canvas.create_text(250, 250, text=f"Ви пропустили {score.lost} яєць.", font=('Helvetica', 20), fill='red')
 tk.update()
 time.sleep(1)
-
-
-
-tk.update()
-time.sleep(3)
