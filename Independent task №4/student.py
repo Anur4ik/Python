73bf59834b87c7ab943654afbb3ee1f73927a41a
nasty <anonim@gmail.com>
2025-10-22 22:32:21 +0300

    created class Student

---
+++ Independent task №4/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, pib, group_number, birth_date="", address=""):
+        self.pib = pib
+        self.group_number = group_number
+        self.birth_date = birth_date
+        self.address = address
