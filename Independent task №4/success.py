9d70cb48679618f208b7929662a35527a557b839
nasty <anonim@gmail.com>
2025-10-22 22:49:24 +0300

    created abstract class success

---
+++ Independent task №4/success.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class Success(ABC):
+    def __init__(self, subjects, grades):
+        self.subjects = subjects
+        self.grades = grades
+
+    @abstractmethod
+    def average_score(self):
+        pass
