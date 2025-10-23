3b3ada4b9b60069ff001fe69bd26e90c6bd49198
nasty <anonim@gmail.com>
2025-10-23 08:42:20 +0300

    updated classes in type_success

--- Independent task №4/type_success.py
+++ Independent task №4/type_success.py
@@ -7,7 +7,7 @@ class DesiredSuccess(Success):
     def average_score(self):
         if len(self.grades) == 0:
             return 0
-        return sum(self.grades) / len(self.grades)
+        return round(sum(self.grades) / len(self.grades), 2)
 class RealSuccess(Success):
     def __init__(self, subjects, desired_grades):
         super().__init__(subjects, desired_grades)
@@ -15,4 +15,4 @@ class RealSuccess(Success):
     def average_score(self):
         if len(self.grades) == 0:
             return 0
-        return sum(self.grades) / len(self.grades)
+        return round(sum(self.grades) / len(self.grades), 2)
