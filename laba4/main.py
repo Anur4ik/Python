501224155817aedd6c462e65f5c444d06e9336be
nasty <anonim@gmail.com>
2025-10-28 08:33:34 +0200

    updated run file

--- laba4/main.py
+++ laba4/main.py
@@ -1,9 +1,11 @@
 from CsvReader import CsvReader
+from DataPreprocessor import DataPreprocessor
 
 csv_url = "https://informer.com.ua/dut/python/import/129-indeksi-tsin-na-zhitlo.csv"
-
-
 csv_reader = CsvReader()
 data = csv_reader.read_data(csv_url)
 
-print(data[:3])
+preprocessor = DataPreprocessor(data)
+processed_data = preprocessor.add_index_ids()
+
+print(processed_data[:3])
