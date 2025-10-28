1e8b448fbfbcdafd5bc813696a87f8975e626d4b
nasty <anonim@gmail.com>
2025-10-28 08:33:54 +0200

    created new class DataAnalyzer

---
+++ laba4/DataAnalyzer.py
@@ -0,0 +1,19 @@
+class DataAnalyzer:
+    def __init__(self, data):
+        self.data = data
+
+    def get_indices(self):
+        indices = [
+            {'ID': entry['_id'], 'period': entry['period'], 'index1': entry['index1'],
+             'index2': entry['index2'], 'index3': entry['index3'], 'index4': entry['index4']}
+            for entry in self.data
+        ]
+        return indices
+
+    def get_periods(self):
+        periods_set = set()
+        for entry in self.data:
+            periods_set.add(entry['period'])
+
+        periods = [{'id': idx + 1, 'period': period} for idx, period in enumerate(periods_set)]
+        return periods
