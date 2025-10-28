1f88c7a00780b1d456b8cc7c172678ca5ff69946
nasty <anonim@gmail.com>
2025-10-28 08:53:48 +0200

    updated run file

--- laba4/main.py
+++ laba4/main.py
@@ -2,7 +2,7 @@ from CsvReader import CsvReader
 from DataPreprocessor import DataPreprocessor
 from DataAnalyzer import DataAnalyzer
 from DataUploader import DataUploader
-
+from DatabaseQueries import DatabaseQueries
 csv_url = "https://informer.com.ua/dut/python/import/129-indeksi-tsin-na-zhitlo.csv"
 
 csv_reader = CsvReader()
@@ -28,4 +28,14 @@ uploader.upload_periods(periods)
 uploader.close()
 
 print("Дані успішно завантажені у базу даних.")
+queries = DatabaseQueries()
+
+indices = queries.find_indices_by_period("2016 Q1")
+print("Індекси за періодом '2016 Q1':", indices)
+
+average_indices = queries.get_average_index_by_period()
+print("Середні значення індексів за періодами:", average_indices)
+
+queries.close()
+
 
