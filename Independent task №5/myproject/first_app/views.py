6d32e28bb466542855b90b7f8dfb989aab7c2e74
Олександр <shr4mm@gmail.com>
2025-11-05 23:45:15 +0200

    .

--- Independent task №5/myproject/first_app/views.py
+++ Independent task №5/myproject/first_app/views.py
@@ -5,17 +5,12 @@ def info_list():
         {'id': 1, 'name': 'Олександр', 'surname': 'Котасов', 'year': '19', 'group': 'ІСД-32','email':'st8483500@stud.duikt.edu.ua'},
     ]
     return info
-
 def table(request):
     html_content = """
-        <h1>Інформація про мене </h1>
+      <h1>Інформація про мене </h1>
       <table border="1" cellpadding="5">           
-        <tr><th>ID</th>
-                    <th>Ім’я</th>
-                    <th>Прізвище</th>
-                    <th>Вік</th>
-                    <th>Група</th>
-                    <th>Email</th>
+        <tr>
+        <th>ID</th><th>Ім’я</th><th>Прізвище</th><th>Вік</th><th>Група</th><th>Email</th>
         </tr>
     """
     for info in info_list():
@@ -27,7 +22,7 @@ def table(request):
                       <td>{info['year']}</td>
                       <td>{info['group']}</td>
                       <td>{info['email']}</td>
-                  </tr>
+                  </tr> 
           """
     html_content += """
      </table>
