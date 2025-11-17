963ad78e944420aab89777913fe91bc3397e4898
Олександр <shr4mm@gmail.com>
2025-11-17 23:26:49 +0200

    independent task 6 completed

--- Independent task №6/bloggers/bloggers_site/urls.py
+++ Independent task №6/bloggers/bloggers_site/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'bloggers'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('bloggers_list/', views.blogger_list, name='bloggers_list'),  # список блогерів
-    path('blogger/<int:id>/', views.blogger_detail, name='blogger_detail'),  # деталі блогера
-    path('news/', views.blogger_news, name='news'),  # сторінка новин
+    path('bloggers/', views.blogger_list, name='bloggers_list'),
+    path('news/', views.blogger_news, name='news'),
+    path('bloggers/i_am/', views.i_am, name='top_bloggers'),
+    re_path(r'^bloggers/(?P<slug>[-a-zA-Z0-9_]+)/$', views.blogger_detail, name='blogger_detail' ),
 ]
--- Independent task №6/bloggers/bloggers_site/views.py
+++ Independent task №6/bloggers/bloggers_site/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blogger, News
 
@@ -7,14 +8,15 @@ def blogger_list(request):
     return render(request, 'bloggers/blogger/list.html', {'bloggers': bloggers})
 
 
-def blogger_detail(request, id):
-    blogger = get_object_or_404(Blogger, id=id, status=Blogger.Status.PUBLISHED)
+def blogger_detail(request, slug):
+    blogger = get_object_or_404(Blogger, slug=slug, status=Blogger.Status.PUBLISHED)
     content_pieces = blogger.content_pieces.all()
     return render(request, 'bloggers/blogger/detail.html', {'blogger': blogger,
         'content_pieces': content_pieces})
 def home(request):
-    latest_news = News.published.all()[:5]
-    return render(request, 'bloggers/home.html', {'latest_news': latest_news})
-def blogger_news(request):
     all_news = News.published.all()
-    return render(request, 'bloggers/news.html', {'all_news': all_news})
+    return render(request, 'bloggers/home.html', {'all_news': all_news})
+def blogger_news(request):
+    return redirect('bloggers:home')
+def i_am(request):
+    return HttpResponse("<h1>Сторінка зі мною</h1><p>Цей маршрут спрацював, тому що він перший в урл але інформації про мене немає.</p>")
