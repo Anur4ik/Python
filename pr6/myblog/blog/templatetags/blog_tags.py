d7fc5b17fe3a65f536889d5e6c6e249b35d087d1
nasty <anonim@gmail.com>
2025-11-25 15:38:28 +0200

    .

--- pr6/myblog/blog/templatetags/blog_tags.py
+++ pr6/myblog/blog/templatetags/blog_tags.py
@@ -2,7 +2,7 @@ from django import template
 from django.db.models import Count
 from django.utils.safestring import mark_safe
 from ..models import Post
-
+import markdown
 register = template.Library()
 
 @register.simple_tag
@@ -18,7 +18,7 @@ def show_latest_posts(count=5):
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
-import markdown
+
 @register.filter(name='markdown')
 def markdown_format(text):
     return mark_safe(markdown.markdown(text))
